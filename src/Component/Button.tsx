0b5d20c6b897b1b71f054e71bb49033f8e600224
Piyush Arya <piyush@mithyalabs.com>
2022-07-03 00:27:16 +0530

    Add button

--- src/Component/Button.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-interface IButtonProps {}
-
-const Button: React.FunctionComponent<IButtonProps> = (props) => {
-  return <Button>Button</Button>;
-};
-
-export default Button;
---
+++ src/Component/Button/index.tsx
@@ -0,0 +1,28 @@
+import clsx from "clsx";
+import * as React from "react";
+
+interface IButtonProps {
+  primary?: boolean;
+  secondary?: boolean;
+  tertiary?: boolean;
+  disabled?: boolean;
+  text: string;
+  classes: string;
+  size: "sm" | "md" | "lg" | "full-width";
+}
+
+const Button: React.FunctionComponent<IButtonProps> = (props) => {
+  const { primary, secondary, tertiary, disabled, text, classes, size } = props;
+  const cls = clsx(
+    classes,
+    "btn",
+    primary ? "btn--primary" : "",
+    secondary ? "btn--secondary" : "",
+    tertiary ? "" : "",
+    disabled ? "btn--disabled" : "",
+    size ? `btn--${size}` : ""
+  );
+  return <button className={cls}>{text}</button>;
+};
+
+export default Button;
